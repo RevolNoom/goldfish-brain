0e8c4d019a3bf8a89289509d36c24525bb067de8
Rev <revolnoom7801@gmail.com>
2021-11-07 23:28:07 +0700

    Create two main interfaces

---
+++ Engine.py
@@ -0,0 +1,14 @@
+class Engine:
+    def __init__(self):
+        printf("Broken Engine.")
+
+    def NextBestMove(ChessBoard)
+        printf("zzz...")
+
+
+#class DeriveEngine(Engine):
+#    def __init__(self):
+#        printf("Powerful Engine!")
+#
+#    def NextBestMove(ChessBoard)
+#        printf("BRMM BRMMM")
---
+++ Evaluator.py
@@ -0,0 +1,16 @@
+class Evaluator:
+    def __init__(self):
+        print("Don't use base class Evaluator. Use a Derived version of it!")
+
+    def Evaluate(self, ChessBoard):
+        print("I'm doing nothing! You asked the wrong guy to do your job.")
+
+
+#class DerivedEvaluator(Evaluator):
+#    def __init__(self):
+#        print("Derived")
+#
+#    def Evaluate(self, ChessBoard):
+#        return 1
+    
+# Main Execution point
