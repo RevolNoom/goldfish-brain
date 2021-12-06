14c66575a97946f3da53f0e144d7298ebea64e07
catchinthedark <catchinthedark.myh@gmail.com>
2021-12-06 22:57:05 +0700

    fix negamax

---
+++ Negamax.py
@@ -0,0 +1,39 @@
+import chess
+import Engine
+import Evaluator
+
+CHECKMATE = 1e5
+
+class Negamax(Engine.Engine):
+    _evaluator = Evaluator.EvaluatorSunfish()
+
+    def nega(self, ChessBoard, Depth, Player):
+        # The stop case: king is captured or draw play once
+        # case: check-mate
+        if ChessBoard.is_checkmate():
+            return Player * CHECKMATE
+        # case: Terminal node: Game over
+        if ChessBoard.is_insufficient_material():
+            return 0
+        # case: find till the last children
+        if Depth == 0:
+            return Player * self._evaluator.Evaluate(ChessBoard)
+        # backtrack and find children
+        bestMove = None
+        max_score = -CHECKMATE
+        for legal_move in ChessBoard.legal_moves:
+            move = chess.Move.from_uci(str(legal_move))
+            ChessBoard.push(move)
+            score = -self.nega(ChessBoard, Depth-1, -Player)
+            if score > max_score:
+                max_score = score
+                if Depth == Depth:
+                    bestMove = move
+            ChessBoard.pop()
+        return (bestMove, max_score)
+
+    def findBestMove(self, ChessBoard, Depth):
+        return self.nega(ChessBoard, Depth, 1 if ChessBoard.turn else -1)[0]
+
+n = Negamax()
+n.findBestMove(chess.Board(),3)
